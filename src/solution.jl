2b12dd5aba5bdf4c7081fd09318c2899c31183fa
Kibaek Kim <kibaek.k@gmail.com>
2021-01-22 19:22:21 -0600

    allow tolerance set

--- src/solution.jl
+++ src/solution.jl
@@ -1,8 +1,12 @@
 # This file implements one solution method for the network decomposition
 # using LagrangeDual from DualDecomposition.jl
 
-function init_DD_algo(dn_model::NetDecModel; method::Type{T} = BM.TrustRegionMethod, maxiter::Int64 = 1000)::DD.LagrangeDual where T <: BM.AbstractMethod
-    algo = DD.LagrangeDual(method, maxiter)
+function init_DD_algo(dn_model::NetDecModel; 
+    method::Type{T} = BM.TrustRegionMethod, 
+    maxiter::Int64 = 1000,
+    tol::Float64 = 1e-6)::DD.LagrangeDual where T <: BM.AbstractMethod
+    algo = DD.LagrangeDual(method, maxiter, tol)
+    
     partition = get_partition(dn_model)
     models = get_models(dn_model)
     for i in eachindex(partition)
