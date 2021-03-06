91ac014ca9762f3f1cac3dac4a0e375e3bc909af
Weiqi <weiqizhang64@gmail.com>
2021-03-06 10:32:41 -0600

    fixed examples/acopf.jl

--- examples/acopf.jl
+++ examples/acopf.jl
@@ -10,8 +10,8 @@ optimizer = optimizer_with_attributes(Gurobi.Optimizer, "OutputFlag" => 0)
 file = "./examples/case5.m"
 data = parse_file(file)
 partitions = [[2, 3, 4], [1, 5]]
-# dn_model = decompose(data, partitions, ACRPowerModel, NetDecOPF.build_acopf_with_free_lines)
-dn_model = decompose(data, partitions, SOCBFPowerModel, NetDecOPF.build_socbf_with_free_lines)
+dn_model = decompose(data, partitions, ACRPowerModel, NetDecOPF.build_acopf_with_free_lines)
 algo = init_DD_algo(dn_model)
 set_subnet_optimizer!(dn_model, sub_optimizer)
-DD.run!(algo, optimizer)
+LM = DD.BundleMaster(BM.ProximalMethod, optimizer)
+DD.run!(algo, LM)
