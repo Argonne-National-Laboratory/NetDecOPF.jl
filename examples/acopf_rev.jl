23e8f57c7da46d440fe05e56162cec3e8024d382
Kibaek Kim <kibaek.k@gmail.com>
2021-08-02 12:24:56 -0500

    Update acopf_rev.jl

--- examples/acopf_rev.jl
+++ examples/acopf_rev.jl
@@ -39,27 +39,28 @@ file = dir_to_pglib * "pglib_opf_case30_ieee.m"
 data = parse_file(file)
 
 # i = 320
-partitions = [[bus["bus_i"]] for (_,bus) in data["bus"]]
-# partitions = metis_cluster(file, 2)
+# partitions = [[bus["bus_i"]] for (_,bus) in data["bus"]]
+partitions = metis_cluster(file, 2)
 
 # This is decomposing with full W matrix as consensus
 myforms = [NonconvexACRModel, W_global_ACRModel]
-dn_model = decompose(data, partitions, myforms[1], NetDecOPF.build_acopf_with_free_lines, extra_ref_extensions=[NetDecOPF.ref_add_global_bus!])
+dn_model = decompose(data, partitions, myforms[2], NetDecOPF.build_acopf_with_free_lines, extra_ref_extensions=[NetDecOPF.ref_add_global_bus!])
 
 # initialization
-for pm in dn_model.model_list
-    m = pm.model
-    for v in JuMP.all_variables(m)
-        if JuMP.has_lower_bound(v) && JuMP.has_upper_bound(v)
-            JuMP.set_start_value(v, (JuMP.lower_bound(v) + JuMP.upper_bound(v)) / 2)
-        end
-    end
-end
+# for pm in dn_model.model_list
+#     m = pm.model
+#     for v in JuMP.all_variables(m)
+#         if JuMP.has_lower_bound(v) && JuMP.has_upper_bound(v)
+#             JuMP.set_start_value(v, (JuMP.lower_bound(v) + JuMP.upper_bound(v)) / 2)
+#         end
+#     end
+# end
 
 algo = init_DD_algo(dn_model)
 set_subnet_optimizer!(dn_model, sub_optimizer)
 LM = DD.BundleMaster(BM.ProximalMethod, optimizer)
 
+BM.set_parameter(LM.params, "ncuts_per_iter", 1)
 BM.set_parameter(LM.params, "maxiter", 2)
 DD.run!(algo, LM)
 algo.subsolve_time = []
